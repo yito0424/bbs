c80cf5cb8c71489b5a0f50989918e7a113d26672
yito0424 <yito@mikilab.doshisha.ac.jp>
2021-06-06 00:14:29 +0900

    threadsテーブルとスレッド一覧画面の大枠作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ThreadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [ThreadController::class, 'index'])
+        ->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
