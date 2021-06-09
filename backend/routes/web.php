81806be84f3a12e7ef4069fbfe71b3e45f8db49c
yito0424 <yito@mikilab.doshisha.ac.jp>
2021-06-09 18:23:24 +0900

    記事投稿にに関するルータの作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -21,7 +21,13 @@ Route::get('/', function () {
 Route::get('/dashboard', [ThreadController::class, 'index'])
         ->middleware(['auth'])->name('dashboard');
 
+Route::get('/threads/new', [ThreadController::class, 'create'])
+        ->middleware(['auth'])->name('thread.create');
+
 Route::get('/threads/{id}', [ThreadController::class, 'show'])
         ->middleware(['auth'])->name('thread.show');
+
+Route::post('/threads', [ThreadController::class, 'store'])
+        ->middleware(['auth'])->name('thread.store');
 
 require __DIR__.'/auth.php';
