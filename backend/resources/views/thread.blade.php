fd60124efe9adbfb7b6e749b07b0f52650e67e6a
yito0424 <yito@mikilab.doshisha.ac.jp>
2021-06-11 16:01:38 +0900

    記事詳細画面に返信フォームを追加

--- backend/resources/views/thread.blade.php
+++ backend/resources/views/thread.blade.php
@@ -10,9 +10,27 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <p class="text-2xl m-4">{{ $thread->content }}</p>
-                    <a href='{{ route("dashboard") }}' class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                        記事一覧へ戻る
-                    </a>
+                    <form method="POST" action="{{ route('thread.store') }}">
+                        @csrf
+
+                        <div class="mt-4">
+                            <x-label for="content" value="内容" />
+
+                            <x-input id="content" class="block mt-1 w-full"
+                                            type="text"
+                                            name="content" />
+                        </div>
+                        <div class="flex items-center justify-end mt-4">
+                            <button type="submit" class="inline-block items-center bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                            スレッドに返信する
+                            </button>
+                        </div>
+                    </form>
+                    <div class="flex items-center justify-end mt-4">
+                        <a href='{{ route("dashboard") }}' class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                            記事一覧へ戻る
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
